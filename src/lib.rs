0b72525baa5fd74a01147cb1f9b64a0469f0b9bc
andriyDev <andriydzikh@gmail.com>
2023-04-02 22:50:29 -0700

    Add a basic test for Detour.

--- src/lib.rs
+++ src/lib.rs
@@ -24,10 +24,10 @@ pub use ffi_recast::*;
 
 #[cfg(test)]
 mod tests {
-  use crate::{ffi_inline::*, ffi_recast::*};
+  use crate::{ffi_detour::*, ffi_inline::*, ffi_recast::*};
 
   #[test]
-  fn create_simple_nav_mesh() {
+  fn recast_create_simple_nav_mesh() {
     let context = unsafe { CreateContext(false) };
     let heightfield = unsafe { rcAllocHeightfield() };
 
@@ -144,5 +144,155 @@ mod tests {
     assert_eq!(vert_slice, expected_verts);
 
     unsafe { rcFreePolyMesh(mesh) };
+  }
+
+  #[test]
+  fn detour_finds_simple_path() {
+    let verts = vec![
+      1, 0, 1, //
+      1, 0, 0, //
+      2, 0, 0, //
+      2, 0, 1, //
+      3, 0, 1, //
+      3, 0, 2, //
+      2, 0, 2, //
+      1, 0, 2, //
+      0, 0, 2, //
+      0, 0, 1, //
+    ];
+
+    const N: u16 = RC_MESH_NULL_IDX;
+
+    let polys = vec![
+      0, 1, 2, N, N, 1, //
+      2, 3, 0, N, 2, 0, //
+      0, 3, 6, 1, 4, 3, //
+      0, 6, 7, 2, N, 6, //
+      6, 3, 4, 2, N, 5, //
+      6, 4, 5, 4, N, N, //
+      0, 7, 8, 3, N, 7, //
+      0, 8, 9, 6, N, N, //
+    ];
+
+    let poly_flags = vec![1, 1, 1, 1, 1, 1, 1, 1];
+    let poly_areas = vec![0, 0, 0, 0, 0, 0, 0, 0];
+
+    let mut nav_mesh_create_data = dtNavMeshCreateParams {
+      verts: verts.as_ptr(),
+      vertCount: verts.len() as i32,
+      polys: polys.as_ptr(),
+      polyFlags: poly_flags.as_ptr(),
+      polyAreas: poly_areas.as_ptr(),
+      polyCount: polys.len() as i32 / 6,
+      nvp: 3,
+      detailMeshes: std::ptr::null(),
+      detailVerts: std::ptr::null(),
+      detailVertsCount: 0,
+      detailTris: std::ptr::null(),
+      detailTriCount: 0,
+      offMeshConVerts: std::ptr::null(),
+      offMeshConRad: std::ptr::null(),
+      offMeshConFlags: std::ptr::null(),
+      offMeshConAreas: std::ptr::null(),
+      offMeshConDir: std::ptr::null(),
+      offMeshConUserID: std::ptr::null(),
+      offMeshConCount: 0,
+      userId: 0,
+      tileX: 0,
+      tileY: 0,
+      tileLayer: 0,
+      bmin: [0.0, 0.0, 0.0],
+      bmax: [3.0, 2.0, 2.0],
+      walkableHeight: 1.0,
+      walkableRadius: 1.0,
+      walkableClimb: 1.0,
+      cs: 1.0,
+      ch: 1.0,
+      buildBvTree: false,
+    };
+
+    let mut data: *mut u8 = std::ptr::null_mut();
+    let mut data_size: i32 = 0;
+
+    assert!(unsafe {
+      dtCreateNavMeshData(&mut nav_mesh_create_data, &mut data, &mut data_size)
+    });
+
+    let nav_mesh = unsafe { &mut *dtAllocNavMesh() };
+    assert_ne!(nav_mesh as *mut dtNavMesh, std::ptr::null_mut());
+    assert_eq!(
+      unsafe { nav_mesh.init1(data, data_size, dtTileFlags_DT_TILE_FREE_DATA) },
+      DT_SUCCESS
+    );
+
+    let nav_mesh_query = unsafe { &mut *dtAllocNavMeshQuery() };
+    assert_ne!(nav_mesh as *mut dtNavMesh, std::ptr::null_mut());
+    assert_eq!(unsafe { nav_mesh_query.init(nav_mesh, 512) }, DT_SUCCESS);
+
+    fn find_poly_ref(query: &dtNavMeshQuery, pos: &[f32; 3]) -> dtPolyRef {
+      let extents = [0.1, 100.0, 0.1];
+
+      let mut path: dtPolyRef = 0;
+
+      let query_filter = dtQueryFilter {
+        m_areaCost: [1.0; 64],
+        m_includeFlags: 0xffff,
+        m_excludeFlags: 0,
+      };
+
+      assert_eq!(
+        unsafe {
+          query.findNearestPoly(
+            pos.as_ptr(),
+            extents.as_ptr(),
+            &query_filter,
+            &mut path,
+            std::ptr::null_mut(),
+          )
+        },
+        DT_SUCCESS
+      );
+
+      path
+    }
+
+    let start_point = [1.1, 0.0, 0.1];
+    let end_point = [2.9, 0.0, 1.9];
+
+    let start_poly_ref = find_poly_ref(&nav_mesh_query, &start_point);
+    let end_poly_ref = find_poly_ref(&nav_mesh_query, &end_point);
+
+    assert_eq!(start_poly_ref, 8);
+    assert_eq!(end_poly_ref, 13);
+
+    let query_filter = dtQueryFilter {
+      m_areaCost: [1.0; 64],
+      m_includeFlags: 0xffff,
+      m_excludeFlags: 0,
+    };
+
+    let mut path = [0; 10];
+    let mut path_count = 0;
+
+    assert_eq!(
+      unsafe {
+        nav_mesh_query.findPath(
+          start_poly_ref,
+          end_poly_ref,
+          start_point.as_ptr(),
+          end_point.as_ptr(),
+          &query_filter,
+          path.as_mut_ptr(),
+          &mut path_count,
+          path.len() as i32,
+        )
+      },
+      DT_SUCCESS
+    );
+
+    assert_eq!(path, [8, 9, 10, 12, 13, 0, 0, 0, 0, 0]);
+
+    unsafe { dtFreeNavMeshQuery(nav_mesh_query) };
+    unsafe { dtFreeNavMesh(nav_mesh) };
   }
 }
